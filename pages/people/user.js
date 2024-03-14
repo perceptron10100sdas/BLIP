1815f7e267c9abc656c5b429e8b6624fc66840ef
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2024-03-14 23:30:09 +0530

    jsut

--- pages/people/user.js
+++ pages/people/user.js
@@ -16,7 +16,7 @@ export default function User({user,id}) {
       onClick={() => router.push(`/profile/${user?.data()?.uid}`)}
       className=" w-full  ring-1 ring-white relative overflow-hidden group bg-slate-200 p-7  rounded-2xl"
     >
-      <div className="absolute inset-0 bg-lime-500 brightness-125 translate-y-[60%] group-hover:translate-y-[0%]  transition-transform duration-300  rounded-3xl rotate-6" />
+      <div className="absolute inset-0 bg-lime-500 brightness-150 translate-y-[60%] group-hover:translate-y-[0%]  transition-transform duration-300  rounded-3xl rotate-6" />
       
       <img src={user?.data()?.userImg} width="50" height="50" className="mb-2  ring-2 ring-black group-hover:ring-white transition-colors relative z-10 duration-300 rounded-2xl" />
      
@@ -28,11 +28,11 @@ export default function User({user,id}) {
       @{user?.data()?.username}
       </p>
       <div className='flex justify-center space-x-5 '>
-      <p className="text-transparent text-center group-hover:text-lime-500 group-hover:brightness-150 bg-transparent group-hover:bg-black p-3 rounded-3xl relative z-10 duration-300 group-hover:white group-hover:text-xl group-hover:mb-3 ">
+      <p className="text-transparent text-center group-hover:text-white group-hover:brightness-150 bg-transparent group-hover:bg-black p-3 rounded-3xl relative z-10 duration-300 group-hover:white group-hover:text-xl group-hover:mb-3 ">
       
      Followers 10100
       </p>
-      <p className="text-transparent text-center group-hover:text-lime-500 group-hover:brightness-150 bg-transparent group-hover:bg-black p-3 rounded-3xl relative z-10 duration-300 group-hover:white group-hover:text-xl group-hover:mb-3 ">
+      <p className="text-transparent text-center group-hover:text-lime-400 group-hover:brightness-150 bg-transparent group-hover:bg-black p-3 rounded-3xl relative z-10 duration-300 group-hover:white group-hover:text-xl group-hover:mb-3 ">
       
      Following 17
       </p></div>
